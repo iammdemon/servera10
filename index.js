eb75d1d779d4b6c71849169f63188f2e70a7ca60
Md. Emon Howlader <emonhowlader.r@gmail.com>
2023-12-04 19:16:24 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+
+app.use(cors());
+app.use(express.json());
+
+// MongoDB
+
+
+
+const uri = `mongodb+srv://admin:kVy2UjxZqSV2DXnW@cluster0.8dckhcl.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const productCollection = client.db('productDB').collection('product');
+
+
+    
+
+    app.post('/add', async(req, res) => {
+        const newProduct = req.body;
+        console.log(newProduct);
+        const result = await productCollection.insertOne(newProduct);
+        res.send(result);
+    })
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send("Intel Brand Shop is running")
+})
+
+
+app.listen(port, () => {
+    console.log("Intel Server Running at " + port)
+})
